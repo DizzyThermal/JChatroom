f4cedffb8e15e4d121e3d858091c831ab5a61f6e
Stephen Diniz <DizzyThermal@gmail.com>
2013-04-22 18:40:55 -0400

    Updated the Resource File

--- JChatroom/src/Resource.java
+++ JChatroom/src/Resource.java
@@ -3,6 +3,13 @@ public class Resource
 	public static final String VERSION_NUMBER		= "v0.0.2";
 	public static final String VERSION_CODENAME		= "Blastoise";
 	
-	public static final String IP					= "localhost";
-	public static final String PORT					= "8010";
+	public static String IP							= "localhost";
+	public static String PORT						= "8010";
+	
+	public static String FILE_SAVE_DIR 				= "C:\\Users\\" + getUsername() + "\\Downloads";
+	
+	public static String getUsername()
+	{
+		return System.getProperty("user.name");
+	}
 }
