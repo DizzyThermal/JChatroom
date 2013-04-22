e60159b2ce6ad649da630165bd490a2ed8dac80b
Stephen Diniz <DizzyThermal@gmail.com>
2013-04-22 01:53:35 -0400

    Lots Done.. Nuff said

--- JChatroom/src/Resource.java
+++ JChatroom/src/Resource.java
@@ -1,5 +1,8 @@
 public class Resource
 {
-	public static final String VERSION_NUMBER		= "v0.0.1";
-	public static final String VERSION_CODENAME		= "Articuno";
+	public static final String VERSION_NUMBER		= "v0.0.2";
+	public static final String VERSION_CODENAME		= "Blastoise";
+	
+	public static final String IP					= "localhost";
+	public static final String PORT					= "8010";
 }
