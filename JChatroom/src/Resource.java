ef20835c13ed178e99eb872240b610cc2cec2a34
Stephen Diniz <DizzyThermal@gmail.com>
2013-04-23 22:13:25 -0400

    Version Update

--- JChatroom/src/Resource.java
+++ JChatroom/src/Resource.java
@@ -1,7 +1,7 @@
 public class Resource
 {
-	public static final String VERSION_NUMBER		= "v0.0.3";
-	public static final String VERSION_CODENAME		= "Charizard";
+	public static final String VERSION_NUMBER		= "v0.0.4";
+	public static final String VERSION_CODENAME		= "Dugtrio";
 	
 	public static String IP							= "192.168.1.100";//"localhost";
 	public static String PORT						= "8010";
